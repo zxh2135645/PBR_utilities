6bfd24fcb9086b7ff37e5c378de22706e2effb3e
James zhang <zxh2135645@gmail.com>
2017-02-12 19:01:40 -0800

    Updated on Feb 12

--- siena_plotting.py
+++ siena_plotting.py
@@ -4,9 +4,10 @@ author: James Zhang
 
 import matplotlib.pyplot as plt
 from math import floor, ceil
+from matplotlib.pyplot import grid
 
 
-f = open('/data/henry7/PBR/subjects/ms1244/siena/PBVC.txt')
+f = open('/Users/jameszhang/PBR_utilities/PBVC.txt')
 lines = f.readlines()
 print(lines)
 f.close()
@@ -18,9 +19,32 @@ for idx in lines:
 print(PBVC_list)
     
 x_axis = list(range(1, len(lines)+1))
-plt.plot(x_axis, PBVC_list, 'ro')
-plt.axis([0, len(lines)+2, floor(min(PBVC_list)), ceil(max(PBVC_list))])
+fig, ax = plt.subplots()
+ax.plot(x_axis, PBVC_list, 'ro')
+ax.axis([0, len(lines)+2, floor(min(PBVC_list)), ceil(max(PBVC_list))])
+grid()
+ax.axhline(y=0, color='b')
+plt.xlabel('Mse Pairs Through Time')
+plt.ylabel('Percentage of Brain Volume Change')
 plt.show()
+
+"""
+#matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0.2,10,100)
+fig, ax = plt.subplots()
+ax.plot(x, 1/x)
+ax.plot(x, np.log(x))
+ax.set_aspect('equal')
+ax.grid(True, which='both')
+
+ax.axhline(y=0, color='k')
+ax.axvline(x=0, color='k')
+plt.show()
+"""
+
 
 
 
