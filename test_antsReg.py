a16b6abf09b66e2eeb888615c385a3135af8fb75
zxh2135645 <zxh2135645@gmail.com>
2017-05-12 16:10:19 -0700

    test antsRegistration

---
+++ test_antsReg.py
@@ -0,0 +1,37 @@
+__author__ = 'sf713420'
+
+if __name__ == '__main__':
+    from nipype.interfaces.ants import Registration
+    from os.path import join
+    working_dir = '/data/henry7/james/test_antsReg'
+    reg = Registration()
+    reg.inputs.moving_image = join(working_dir, 'ms1244-mse5466-040-MPRAGE_64channel_p2.nii.gz')
+    reg.inputs.fixed_image = join(working_dir, 'ms1244-mse3670-040-MPRAGE_64channel_p2.nii.gz')
+    reg.inputs.output_transform_prefix = "output_"
+    reg.inputs.transforms = ['Rigid', 'Affine', 'SyN']
+    reg.inputs.transform_parameters = [(0.1,), (0.1,), (0.2, 3.0, 0.0)]
+    reg.inputs.number_of_iterations = [[10000, 11110, 11110]] * 2 + [[100, 30, 20]]
+    reg.inputs.dimension = 3
+    reg.inputs.write_composite_transform = False  #False or True?
+    reg.inputs.collapse_output_transforms = False
+    reg.inputs.initial_moving_transform_com = True
+    reg.inputs.metric = ['Mattes'] * 2 + [['Mattes', 'CC']]
+    reg.inputs.metric_weight = [1] * 2 + [[0.5, 0.5]]
+    reg.inputs.radius_or_number_of_bins = [32] * 2 + [[32, 4]]
+    reg.inputs.sampling_strategy = ['Regular'] * 2 + [[None, None]]
+    reg.inputs.sampling_percentage = [0.3] * 2 + [[None, None]]
+    reg.inputs.convergence_threshold = [1.e-8] * 2 + [-0.01]
+    reg.inputs.convergence_window_size = [20] * 2 + [5]
+    reg.inputs.smoothing_sigmas = [[4, 2, 1]] * 2 + [[1, 0.5, 0]]
+    reg.inputs.sigma_units = ['vox'] * 3
+    reg.inputs.shrink_factors = [[3, 2, 1]]*2 + [[4, 2, 1]]
+    reg.inputs.use_estimate_learning_rate_once = [True] * 3
+    reg.inputs.use_histogram_matching = [False] * 2 + [True]
+    reg.inputs.winsorize_lower_quantile = 0.005
+    reg.inputs.winsorize_upper_quantile = 0.995
+    reg.inputs.args = '--float'
+    reg.inputs.output_warped_image = 'output_warped_image.nii.gz'
+    reg.inputs.num_threads = 4
+    reg.plugin_args = {'sbatch_args': '-c%d' % 4}
+    print(reg.cmdline)
+    reg.run()
