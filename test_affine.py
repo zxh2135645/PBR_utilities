2e664210f89417e4800fc79a50121bdad0d89b60
zxh2135645 <zxh2135645@gmail.com>
2017-05-11 18:31:21 -0700

    text affine transform

--- test_affine.py
+++ test_affine.py
@@ -4,7 +4,13 @@ from glob import glob
 import numpy as np
 import scipy.io as spio
 
-affine = glob('/data/henry7/PBR/subjects/ms1244/t1Ants_reg_long/mse3670__mse3633/*Affine*')
+affine = glob('/data/henry7/PBR/subjects/ms1244/t1Ants_reg_long/mse5466__mse3670/output_0GenericAffine.mat')
 affine = ''.join(affine)
 matdata = spio.loadmat(affine)
 print(matdata)
+print(matdata["fixed"])
+print(matdata["AffineTransform_float_3_3"])
+
+convertToAffineType = '/data/henry7/PBR/subjects/ms1244/t1Ants_reg_long/mse5466__mse3670/matrix_convertToAffineType.mat'
+matdata2 = spio.loadmat(convertToAffineType)
+print(matdata2)
