d016049cd831b6cb535af8e3d977668c411d85e9
zxh2135645 <zxh2135645@gmail.com>
2017-05-17 01:34:33 -0700

    get median of gray matter

--- test_hist.py
+++ test_hist.py
@@ -21,22 +21,32 @@ labels = [4, 5, 14, 15, 24, 43, 44, 72]
 # 24 is CSF
 # 72 is Fifth-Ventricle
 
+labels2 = [2005, 2008, 2010, 2011, 2014, 2015, 2018, 2019, 2020, 2021, 2024, 2025, 2026, 2027, 2029, 2030, 2031, 2032, 2033, 2034,
+           1005, 1008, 1010, 1012, 1014, 1018, 1019, 1020, 1022, 1024, 1025, 1026, 1027, 1029, 1031, 1032, 1035, 1032]
+
 ventricle = np.where(np.logical_or.reduce([data == i for i in labels]))
-print(ventricle)
+gray_matter = np.where(np.logical_or.reduce([data == i for i in labels2]))
+# print(ventricle)
 
 img2 = nib.load('/data/henry7/PBR/subjects/mse5466/alignment/ms1244-mse5466-040-MPRAGE_64channel_p2.nii.gz')
 data2, aff = img2.get_data(), img2.affine
 print("The image is: ", data2[ventricle])
 
 ventricle_median = np.median(data2[ventricle])
-print("The median intensity of ventricle is:", ventricle_median)
+gray_matter_median = np.median(data2[gray_matter])
+print("The median intensity of ventricle is:", ventricle_median,
+      "The median intensity of gray matter is:", gray_matter_median)
 
 mask = np.zeros(data2.shape)
 mask[ventricle] = 1
+mask2 = np.zeros(data2.shape)
+mask2[gray_matter] = 1
 
 #TODO How to find gray matters
-#mask_out = nib.Nifti1Image(mask, affine=aff)
+#gray matters were roughly found
+mask_out = nib.Nifti1Image(mask2, affine=aff)
 #nib.save(mask_out, '/data/henry6/PBR//surfaces/ms1244-mse5466-040-MPRAGE_64channel_p2/mri/ventricle_mask.nii.gz')
+nib.save(mask_out, '/data/henry6/PBR//surfaces/ms1244-mse5466-040-MPRAGE_64channel_p2/mri/gray_matter_mask.nii.gz')
 
 #labeled_img, nlabels = label(data > 0)
 #print("The sum is: ", np.sum(labeled_img))
