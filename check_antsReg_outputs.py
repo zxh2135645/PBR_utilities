ae3b1c2101c206868390cd0e820ad09f25c15672
zxh2135645 <zxh2135645@gmail.com>
2017-05-19 01:34:17 -0700

    minor changes to complete the code

--- check_antsReg_outputs.py
+++ check_antsReg_outputs.py
@@ -1,5 +1,5 @@
 from glob import glob
-from nipype.utils.filemanip import load_json
+from nipype.utils.filemanip import load_json, save_json
 import os
 import pandas as pd
 
@@ -16,13 +16,17 @@ if __name__ == '__main__':
     for msid in msid_set_sorted:
         check_status = glob(os.path.join(outdir, msid, 't1Ants_reg_long', 'status.json'))
         if len(check_status) == 0:
-            check['not_run_yet'] = msid
+            check['not_run_yet'].append(msid)
         else:
+            check_status = ''.join(check_status)
             status = load_json(check_status)
-            if len(status['mseID']) == len(status['fixed_image']) + 1:
-                check['finished'] = msid
+            if len(status['mseIDs']) == len(status['fixed_image']) + 1:
+                check['finished'].append(msid)
             else:
-                check['need_rerun'] == msid
+                check['need_rerun'].append(msid)
+    save_json('/data/henry7/james/antje_cohort/check_pbr_outputs.json', check)
+
+
 
 
 
