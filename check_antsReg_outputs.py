9a2db6de17e30a54d0aa1f406a165c9fe430d269
zxh2135645 <zxh2135645@gmail.com>
2017-05-20 16:21:55 -0700

    minor modifications

--- check_antsReg_outputs.py
+++ check_antsReg_outputs.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 from glob import glob
 from nipype.utils.filemanip import load_json, save_json
 import os
@@ -18,12 +19,14 @@ if __name__ == '__main__':
         if len(check_status) == 0:
             check['not_run_yet'].append(msid)
         else:
+            print(check_status)
             check_status = ''.join(check_status)
             status = load_json(check_status)
             if len(status['mseIDs']) == len(status['fixed_image']) + 1:
                 check['finished'].append(msid)
             else:
                 check['need_rerun'].append(msid)
+    print(check)
     save_json('/data/henry7/james/antje_cohort/check_pbr_outputs.json', check)
 
 
