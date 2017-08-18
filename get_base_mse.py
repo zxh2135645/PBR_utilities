6428437430c7aa7f142e5b20bf0ef31185cfee71
zxh2135645 <zxh2135645@gmail.com>
2017-08-18 02:19:00 -0700

    minor modification

--- get_base_mse.py
+++ get_base_mse.py
@@ -31,24 +31,29 @@ if __name__ == '__main__':
     path_list = glob('/data/henry6/mindcontrol_ucsf_env/watchlists/long/VEO/test/ms*.txt')
     ms_list = [ms.split('/')[-1].split('.')[0] for ms in path_list]
     status = pd.DataFrame()
+    status['msid'] = pd.Series(ms_list)
 
     for msid in ms_list:
-        mse_list = pd.read_table('/data/henry6/mindcontrol_ucsf_env/watchlists/long/VEO/test/{0}.txt'.format(msid))
-        mseid = mse_list[0]
-        print(mse_list[0])
-        for mse_idx, mse in enumerate(mseid):
+        mse_list = pd.read_table('/data/henry6/mindcontrol_ucsf_env/watchlists/long/VEO/test/{0}.txt'.format(msid),
+                                 header=None)
+        mseid = mse_list.iloc[::-1]
+        print(mseid)
+        for mse in mseid[0]:
+            print(mse)
             lst_edit_check = glob(os.path.join(config["output_directory"], mse, 'mindcontrol', '*FLAIR*',
-                                           'lst', 'lst_edits', 'no_FP_filled_FN_dr2*'))
+                                               'lst', 'lst_edits', 'no_FP_filled_FN_dr2*.nii.gz'))
             if len(lst_edit_check) == 1:
                 lesion_edit = ''.join(lst_edit_check)
                 break
             elif len(lst_edit_check) > 1:
                 raise ValueError("lst_edits files have more than one inputs, please check PBROUT directory",
                                  os.path.split(lst_edit_check[0]))
-            elif len(lst_edit_check) == 0:
-                raise ValueError("lst_edits files do not have edited lesion in the mindcontrol for msid:",
-                                 "\n {0}".format(msid))
-        status['msid'] = msid
-        status['mse'] = mse
+
+        if len(lst_edit_check) == 0:
+            status.loc[status['msid'] == msid, 'mse'] = 'NA'
+            print("lst_edits files do not have edited lesion in the mindcontrol for msid:",
+                             "\n {0}".format(msid))
+        else:
+            status.loc[status['msid'] == msid, 'mse'] = mse
 
     status.to_csv('/data/henry7/james/antje_cohort/baseline_mse.csv')
